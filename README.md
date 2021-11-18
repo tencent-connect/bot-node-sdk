edcc8b2012a86273ba92a14848389896fbcc5610
vnues <1518190293@qq.com>
2021-11-18 17:16:52 +0800

    chore: optimized engineer

---
+++ README.md
@@ -0,0 +1,13 @@
+# bot-node-sdk
+
+## 如何贡献
+
+```shell
+// 本地运行
+npm run dev
+npm link
+// cd exmaple
+npm link @tencent/bot-node-sdk
+// 调试
+npm run example
+```
---
+++ rollup.config.js
@@ -0,0 +1,73 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import replace from '@rollup/plugin-replace';
+import dts from 'rollup-plugin-dts';
+
+const ENV = process.env.NODE_ENV;
+const extensions = ['.ts', '.js'];
+const external = ['form-data', 'lodash.clonedeep', 'setimmediate', 'utf-8-validate', 'ws'];
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'lib/index.js',
+      format: 'cjs',
+      sourcemap: ENV === 'dev',
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        extensions,
+        babelHelpers: 'runtime',
+      }),
+      nodeResolve({ browser: true, extensions }),
+      typescriptPaths({
+        preserveExtensions: true,
+      }),
+      commonjs(),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify(ENV),
+      }),
+    ],
+    external,
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'es/index.js',
+      format: 'es',
+      sourcemap: ENV === 'dev',
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        extensions,
+        babelHelpers: 'runtime',
+      }),
+      nodeResolve({ browser: true, extensions }),
+      typescriptPaths({
+        preserveExtensions: true,
+      }),
+      commonjs(),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify(ENV),
+      }),
+    ],
+    external,
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'typings/index.d.ts', format: 'es' }],
+    plugins: [
+      typescriptPaths({
+        preserveExtensions: true,
+      }),
+      dts(),
+    ],
+  },
+];
