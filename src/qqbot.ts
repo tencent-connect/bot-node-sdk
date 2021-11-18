e5c2260405d3d1e88d54f5ef7dbf226b19e7c453
joyqwang <joyqwang@tencent.com>
2021-11-18 19:34:03 +0800

    feat: 心跳重连

--- src/qqbot.ts
+++ src/qqbot.ts
@@ -1,34 +1,15 @@
-import { Client } from '../src/websocket/client/client';
 import { Wss } from '../src/websocket/websocket';
 import { GetWssParam, WssAddressObj } from './types/qqbot-types';
-import { HttpsService } from './rest/api-request';
 
 export class QQBot {
+  static get() {
+    console.log('....aaa....');
+  }
   // 新建一个连接
   async newClient(config: GetWssParam) {
-    await HttpsService.getWss(config).then(res => {
-      console.log(`res: ${res}`);
-    })
-    // 模拟数据
-    const testWss: WssAddressObj = {
-      "url": "wss://api.sgroup.qq.com/websocket",
-      "shards": 1,
-      "session_start_limit": {
-        "total": 1000,
-        "remaining": 1000,
-        "reset_after": 86400000,
-        "max_concurrency": 1
-      }
-    }
-
-    const client = new Wss(testWss, config);
+    const client = new Wss(config);
     client.creat();
     // await this.registHolder(client, config.cached);
     return client;
   }
-
-  get() {
-    console.log('....aaa....')
-  }
-};
-
+}
