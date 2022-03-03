e7e752ff223c912d44dac404c31a221bf700bf89
zoswing <1141421776@qq.com>
2022-03-03 15:59:21 +0800

    feat: 添加设置公告推荐子频道API

--- src/types/openapi/v1/announce.ts
+++ src/types/openapi/v1/announce.ts
@@ -6,6 +6,7 @@ import { RestyResponse } from 'resty-client';
 export interface AnnounceAPI {
   postGuildAnnounce: (guildID: string, channelID: string, messageID: string) => Promise<RestyResponse<IAnnounce>>;
   deleteGuildAnnounce: (guildID: string, messageID: string) => Promise<RestyResponse<any>>;
+  postGuildRecommend: (guildID: string, recommendObj: RecommendObj) => Promise<RestyResponse<IAnnounce>>;
   postChannelAnnounce: (channelID: string, messageID: string) => Promise<RestyResponse<IAnnounce>>;
   deleteChannelAnnounce: (channelID: string, messageID: string) => Promise<RestyResponse<any>>;
 }
@@ -15,4 +16,16 @@ export interface IAnnounce {
   guild_id: string; // 频道ID
   channel_id: string; // 子频道ID
   message_id: string; // 消息ID
+  announce_type?: number; // 推荐类别 0:成员公告; 1:欢迎公告
+  recommend_channels?: RecommendChannel[]; // 推荐子频道列表
+}
+
+export interface RecommendObj {
+  announces_type?: number; // 公告类别 0:成员公告，1:欢迎公告，默认为成员公告
+  recommend_channels: RecommendChannel[]; // 推荐子频道列表
+}
+
+export interface RecommendChannel {
+  channel_id: string;
+  introduce: string; // 推荐语
 }
